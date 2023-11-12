8367cd5a42182c275f73ca6b4b0c61f5c6875b7b
sergiughimp <ghimpsergiu@gmail.com>
2023-11-12 18:59:19 +0200

    update homework_session1

--- Session1_variables_data_types/homework_session1.py
+++ Session1_variables_data_types/homework_session1.py
@@ -1,7 +1,7 @@
 '''Exerciții obligatorii - grad de dificultate: Ușor spre Mediu:
 1. În cadrul unui comentariu, explică cu cuvintele tale ce este o variabilă.
 '''
-print("Ex 1")
+print(" *********************************************************** Ex 1")
 print("variable = container to store values")
 '''
 2. Declară și initializează câte o variabilă din fiecare din următoarele tipuri de variabilă :
@@ -11,7 +11,7 @@ print("variable = container to store values")
     - bool
     Observație: Valorile vor fi alese de tine după preferințe.
 '''
-print("Ex 2")
+print(" *********************************************************** Ex 2")
 fruit = "apple"
 number_fruit = 3
 kilograms_fruit = 0.7
@@ -19,7 +19,7 @@ in_basket_fruit = False
 '''
 3. Utilizează funcția type pentru a verifica dacă au tipul de date așteptat.
 '''
-print("Ex 3")
+print(" *********************************************************** Ex 3")
 print(f"fruit {fruit} has data type {type(fruit)}")
 print(f"numer of the fruit {number_fruit} has data type {type(number_fruit)}")
 kilograms_fruit = kilograms_fruit
@@ -29,13 +29,13 @@ print(f"in basket fruit {in_basket_fruit} has data type {type(in_basket_fruit)}"
 4. Rotunjește ‘float’-ul folosind funcția round() și salvează această modificare în aceeași variabilă (suprascriere):
     - Verifică tipul acesteia.
 '''
-print("Ex 4")
+print(" *********************************************************** Ex 4")
 kilograms_fruit = round(kilograms_fruit)
 print(kilograms_fruit)
 '''
 5. Folosește print() și printează în consola 4 propoziții folosind cele 4 variabile. Rezolvă nepotrivirile de tip prin ce modalitate dorești.
 '''
-print("Ex 5")
+print(" *********************************************************** Ex 5")
 print(f"Yesterday I bought a {fruit}")
 print(f"Number of them are {number_fruit}")
 print(f"It weighs {kilograms_fruit} kilograms")
@@ -46,7 +46,7 @@ print(f"It is in the basket {in_basket_fruit}")
     - prenumele.
     Afișează: 'Numele complet are x caractere'.
 '''
-print("Ex 6")
+print(" *********************************************************** Ex 6")
 # first_name = input("Enter first name: ")
 # last_name = input("Enter last name: ")
 
@@ -59,7 +59,7 @@ print(f"Full name is {first_name} {last_name} and it has {len(first_name) + len(
     - lățimea.
     Afișează: 'Aria dreptunghiului este x'.
 '''
-print("Ex 7")
+print(" *********************************************************** Ex 7")
 # length = input("Enter length: ")
 # width = input("Enter width: ")
 
@@ -70,7 +70,7 @@ print(f"The area of triangle is: {length * width}")
 8. Având stringul: 'Coral is either the stupidest animal or the smartest rock':
     - afișează de câte ori apare cuvântul 'the';
 '''
-print("Ex 8")
+print(" *********************************************************** Ex 8")
 coral_string = "Coral is either the stupidest animal or the smartest rock"
 print(coral_string.count("the"))
 '''
@@ -78,13 +78,13 @@ print(coral_string.count("the"))
     ● Afișează de câte ori apare cuvântul 'the';
     ● Printează rezultatul.
 '''
-print("Ex 9")
+print(" *********************************************************** Ex 9")
 # print(coral_string.count("the"))
 '''
 10. Același string.
     ● Folosiți un assert ca să verificați dacă acest string conține doar numere.
 '''
-print("Ex 10")
+print(" *********************************************************** Ex 10")
 # assert coral_string.isdigit() == coral_string, "No numbers"
 s = "444"
 assert s.isdigit(), "no numbers"
@@ -94,7 +94,7 @@ Exerciții Opționale - grad de dificultate: Mediu spre greu (s-ar putea să ai 
     - citește de la tastatură un string de dimensiune impară;
     - afișează caracterul din mijloc.
 '''
-print("Ex 1")
+print(" *********************************************************** Ex 1")
 odd_size_string = "lumos"
 for i in range(0, len(odd_size_string)):
     if int(len(odd_size_string) / 2) == i:
@@ -102,7 +102,7 @@ for i in range(0, len(odd_size_string)):
 '''
 2. Folosind assert, verifică dacă un string este palindrom.
 '''
-print("Ex 2")
+print(" *********************************************************** Ex 2")
 palindrom_string = "odo"
 assert "odo" == palindrom_string[::-1], "it is not palindrom"
 '''
@@ -111,7 +111,7 @@ assert "odo" == palindrom_string[::-1], "it is not palindrom"
     - salvează fiecare cuvânt într-o variabilă;
     - printează ambele variabile pentru verificare.
 '''
-print("Ex 3")
+print(" *********************************************************** Ex 3")
 string = "alabala portocala"
 first_string1, second_string1 = "alabala", "portocala"
 first_string2, second_string2 = string.split()
@@ -124,6 +124,7 @@ print(first_string2, second_string2)
     - salvează primul caracter într-o variabilă - indiferent care este el, încearcă cu 2 stringuri diferite;
     - capitalizează acest caracter peste tot, mai puțin pentru primul și ultimul caracter => alAbAlA portocAla.
 '''
+print(" *********************************************************** Ex 4")
 first_character_string = string[0]
 print(first_character_string)
 for i in range(1, len(string)-1):
@@ -140,6 +141,74 @@ print(string[0] + replace[1:-1] + string[-1])
     eg: parola abc => ***
     parola abcd => ****
 '''
+print(" *********************************************************** Ex 5")
 user = "John"
 password = "11111"
 print(f"The user is {user} and the password is {password.replace(password, "*"*len(password))}. the password has {len(password)} characters")
+
+
+print("*********************************************************** additional exercises")
+print("*********************************************************** len function")
+name = "David"
+print(len(name))
+
+print("*********************************************************** slicing")
+print("*********************************** positive slicing")
+print(name[2]) # the 3d element: v
+print(name[1:3]) # from the 2nd to 3d element: av
+# str[start:end:step]
+print(name[0:len(name):2]) # similar
+print(name[:len(name):2]) # similar
+print(name[0::2]) # similar
+print(name[::2]) # similar
+
+print("*********************************** negative slicing")
+print(name[-1]) # <=> name[len(name)-1]
+print(name[-2])
+print(name[-3])
+
+print(name[::-1]) # reverse string
+
+print("*********************************** methods on string")
+
+s = "Coral is either the stupidest animal or the smartest rock. Next time"
+d = "1234"
+print(s.isdigit())
+print(d.isdigit())
+
+print(s.split("."))
+first_sentence, second_sentence = s.split(".")
+print(first_sentence + ".")
+print(second_sentence)
+
+print(s.count("e")) # how many times "e" is in the string s
+print(s.count(" the "))
+print(s.index("C"))
+print(s.upper())
+print(s.lower())
+print(s.replace(" the ", " *** "))
+
+print("*********************************************************** split function")
+name = "Jackson-Ross Eva-Maria"
+last_name1, first_name1 = name.split()
+print(last_name1)
+print(first_name1)
+middle_name, first_name1 = first_name1.split("-")
+print(middle_name)
+print(first_name1)
+
+print("*********************************************************** type function")
+s = "1234"
+print(type(s))
+
+s = "12.34"
+print(type(s))
+
+s = "True"
+print(type(s))
+print("*********************************************************** assert function")
+
+s = "42"
+assert s.isdigit()
+assert s.isnumeric()
+assert int(s) == 42
